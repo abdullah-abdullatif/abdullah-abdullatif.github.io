c050b6985f7b085cb53ef254b313b07033bce1a7
Abdullah Ahmed <bedo.a.samir@gmail.com>
2023-06-09 16:06:11 +0300

    update

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "bedo.a.samir@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
